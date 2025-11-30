e9c7316e75b75c10a824bcc5c808df252ec96e21
Wayne Stewart <waynegaidin@hotmail.com>
2025-11-29 19:38:53 -0600

    added HttpContext and better request logging

--- dev-server/src/main.rs
+++ dev-server/src/main.rs
@@ -1,10 +1,5 @@
 use std::{
-    fs::File, 
-    io::{Read, Write}, 
-    net::{TcpListener, TcpStream}, 
-    path::PathBuf, 
-    time::Duration,
-    thread::spawn,
+    fs::File, io::{Read, Write}, net::{TcpListener, TcpStream}, path::PathBuf, thread::spawn, time::Duration
 };
 
 const WWWROOT: &str = "../";
@@ -16,13 +11,14 @@ struct ClientConnection {
     request_count: u32,
 }
 
-/*struct HttpContext<'a> {
+struct HttpContext<'a> {
     client: &'a ClientConnection,
+    verb: &'a str,
     path_and_query: &'a str,
     path: &'a str,
-    query: &'a str,
+    _query: &'a str,
     status: u16,
-}*/
+}
 
 fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:8080")?;
@@ -53,8 +49,8 @@ fn handle_client(mut client: ClientConnection) {
     //client.stream.set_nonblocking(true);
     while client.keep_alive {
         client.request_count += 1;
-        match read_request(&mut client) {
-            Err(e) => { log_error(format!("conn id: {}, read_request, {:?}", client.id, e).as_str()); }
+        match handle_request(&mut client) {
+            Err(e) => { log_error(format!("conn id: {}, handle_request, {:?}", client.id, e).as_str()); }
             _ => {}
         }
     }
@@ -64,7 +60,7 @@ fn handle_client(mut client: ClientConnection) {
     }
 }
 
-fn read_request(client: &mut ClientConnection) -> std::io::Result<()> {
+fn handle_request(client: &mut ClientConnection) -> std::io::Result<()> {
     let mut readbuf: [u8; 4096] = [0; 4096];
     let bytes_read = match client.stream.read(&mut readbuf) {
         Ok(s) => s,
@@ -75,28 +71,54 @@ fn read_request(client: &mut ClientConnection) -> std::io::Result<()> {
         return Ok(());
     }
     //println!("{}", String::from_utf8(readbuf[0..bytes_read].to_vec()).unwrap());
-    let is_get = readbuf.starts_with(b"GET ");
-    if !is_get { send_404(&client.stream, "invalid verb!")?; return Ok(()); }
-    let end_of_file_path = readbuf.iter().skip(4).position(|&b| b == b' ').unwrap();
-    let uri = std::str::from_utf8(&readbuf[4..(end_of_file_path+4)]).unwrap();
+    let mut iter = readbuf.split(|&c| c == b' ' || c == b'\r');
+    let verb = std::str::from_utf8(iter.next().unwrap()).unwrap();
+    let path_and_query = std::str::from_utf8(iter.next().unwrap()).unwrap();
+    let _http_version = std::str::from_utf8(iter.next().unwrap()).unwrap();
+    let mut path: &str = path_and_query;
+    let mut query: &str = "";
+    if let Some(query_index) = path_and_query.find('?') {
+        (path, query) = path_and_query.split_at(query_index);
+    }
+    //println!("verb: {}, path: {}, http: {}", verb, path, http_version);
+    let mut http_context = HttpContext {
+        client: client,
+        verb: verb,
+        path_and_query: path_and_query,
+        path: path,
+        _query: query,
+        status: 0,
+    };
+    if http_context.verb.eq("GET") {
+        handle_static_file(&mut http_context)?;
+    }
+    else {
+        send_405(&mut http_context)?
+    }
+    log_request(&http_context);
+    Ok(())
+}
+
+fn handle_static_file(mut http_context: &mut HttpContext) -> std::io::Result<()> {
     let mut path_buf = PathBuf::from(WWWROOT);
-    path_buf.push(uri.trim_start_matches('/'));
-    log_request(format!("{} {} {:?}", client.id, client.request_count, path_buf).as_str());
+    path_buf.push(http_context.path.trim_start_matches('/'));
     match File::open(path_buf) {
-        Ok(file) => { send_file(&client.stream, file, uri)?; },
-        Err(e) => { send_404(&client.stream, &format!("error: {:?}", e))?; }
+        Ok(file) => { send_file(&mut http_context, file)?; },
+        Err(_) => { send_404(&mut http_context)?; }
     };
     Ok(())
 }
 
-fn send_file(mut stream: &TcpStream, mut file: File, uri: &str) -> std::io::Result<()> {
-    let content_type = match uri.split(".").last().unwrap() {
+fn send_file(http_context: &mut HttpContext, mut file: File) -> std::io::Result<()> {
+    let mut stream = &http_context.client.stream;
+    let content_type = match http_context.path.split(".").last().unwrap() {
         "js" => "text/javascript",
         "css" => "text/css",
         "html" => "text/html",
         _ => "",
     };
-    if content_type.len() == 0 { send_404(stream, "file extension not supported!")?; return Ok(()); }
+    if content_type.len() == 0 { send_400(http_context, "file extension not supported!")?; return Ok(()); }
+    http_context.status = 200;
     let mut filebuf: Vec<u8> = Vec::new();
     file.read_to_end(&mut filebuf)?;
     stream.write(b"HTTP/1.1 200 OK\r\n")?;
@@ -107,8 +129,25 @@ fn send_file(mut stream: &TcpStream, mut file: File, uri: &str) -> std::io::Resu
     Ok(())
 }
 
-fn send_404(mut stream: &TcpStream, msg: &str) -> std::io::Result<()> {
-    stream.write(b"HTTP/1.1 404 NotFound\r\nContent-Type: text/html\r\n")?;
+fn send_400(http_context: &mut HttpContext, msg: &str) -> std::io::Result<()> {
+    http_context.status = 400;
+    send_message(&http_context.client.stream, 400, "Bad Request", msg)
+}
+
+fn send_404(http_context: &mut HttpContext) -> std::io::Result<()> {
+    http_context.status = 404;
+    send_message(&http_context.client.stream, 404, "Not Found", "Resource Not Found")
+}
+
+fn send_405(http_context: &mut HttpContext) -> std::io::Result<()> {
+    http_context.status = 405;
+    send_message(&http_context.client.stream, 405, "Method Not Allowed", "Method Not Allowed")
+}
+
+fn send_message(mut stream: &TcpStream, status_code: u16, status_msg: &str, msg: &str) -> std::io::Result<()> {
+    stream.write(format!("HTTP/1.1 {} {}\r\n", status_code, status_msg).as_bytes())?;
+    stream.write(b"Connection: keep-alive\r\n")?;
+    stream.write(format!("Content-Type: text/html\r\n").as_bytes())?;
     stream.write(format!("Content-Length: {}\r\n\r\n{}", msg.len(), msg).as_bytes())?;
     Ok(())
 }
@@ -117,19 +156,21 @@ fn _echo_client(mut stream: TcpStream ) -> std::io::Result<()> {
     let mut buf: [u8; 4096] = [0; 4096];
     let bytes_read = stream.read(&mut buf)?;
     println!("{}", bytes_read);
-    //stream.write_all(&buf)?;
     stream.write(b"HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n")?;
     stream.write(format!("Content-Length: {}\r\n\r\n", bytes_read + 11).as_bytes())?;
     stream.write(b"<pre>")?;
     stream.write(&buf[..bytes_read])?;
     stream.write(b"</pre>")?;
-    
     Ok(())
 }
 
 fn log_error(msg: &str) {
     println!("ERROR: {}", msg);
 }
-fn log_request(msg: &str) {
-    println!("{}", msg);
+fn log_request(ctx: &HttpContext) {
+    println!("{} {} {} {}", 
+        ctx.client.id, 
+        ctx.client.request_count, 
+        ctx.status, 
+        ctx.path_and_query);
 }
