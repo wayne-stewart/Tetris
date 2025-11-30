50f98038ff538268de7413f28673c78b7038684e
Wayne Stewart <waynegaidin@hotmail.com>
2025-11-29 22:54:57 -0600

    made a bunch of updates suggested by clippy

--- webserver/src/main.rs
+++ webserver/src/main.rs
@@ -3,12 +3,13 @@ use std::{
 };
 
 const WWWROOT: &str = "../";
+const WWWHOST: &str = "0.0.0.0:8080";
 
 struct ClientConnection {
     stream: TcpStream,
     keep_alive: bool,
-    id: u32,
-    request_count: u32,
+    id: usize,
+    request_count: usize,
 }
 
 struct HttpContext<'a> {
@@ -21,17 +22,14 @@ struct HttpContext<'a> {
 }
 
 fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:8080")?;
-    let mut client_id_source = 0;
-    for stream in listener.incoming() {
-        client_id_source += 1;
-        let client_id = client_id_source;
+    let listener = TcpListener::bind(WWWHOST)?;
+    for (client_id, stream) in listener.incoming().enumerate() {
         spawn(move || {
             match stream {
                 Err(e) => { log_error(format!("conn id: {}, new stream failed, {:?}", client_id, e).as_str()); },
                 Ok(stream) => {
                     let client = ClientConnection { 
-                        stream: stream, 
+                        stream, 
                         keep_alive: true,
                         id: client_id,
                         request_count: 0,
@@ -48,23 +46,18 @@ fn handle_client(mut client: ClientConnection) {
     client.stream.set_read_timeout(Some(Duration::from_secs(5))).unwrap();
     while client.keep_alive {
         client.request_count += 1;
-        match handle_request(&mut client) {
-            Err(e) => { log_error(format!("conn id: {}, handle_request, {:?}", client.id, e).as_str()); }
-            _ => {}
+        if let Err(e) = handle_request(&mut client) {
+            log_error(format!("conn id: {}, handle_request, {:?}", client.id, e).as_str());
         }
     }
-    match client.stream.shutdown(std::net::Shutdown::Both) {
-        Err(e) => { log_error(format!("conn id: {}, stream.shutdown, {:?}", client.id, e).as_str()); }
-        _ => { }
+    if let Err(e) = client.stream.shutdown(std::net::Shutdown::Both) {
+        log_error(format!("conn id: {}, stream.shutdown, {:?}", client.id, e).as_str());
     }
 }
 
 fn handle_request(client: &mut ClientConnection) -> std::io::Result<()> {
     let mut readbuf: [u8; 4096] = [0; 4096];
-    let bytes_read = match client.stream.read(&mut readbuf) {
-        Ok(s) => s,
-        Err(_e) => 0
-    };
+    let bytes_read = client.stream.read(&mut readbuf).unwrap_or_default();
     if bytes_read == 0 {
         client.keep_alive = false;
         return Ok(());
@@ -80,10 +73,10 @@ fn handle_request(client: &mut ClientConnection) -> std::io::Result<()> {
         (path, query) = path_and_query.split_at(query_index);
     }
     let mut http_context = HttpContext {
-        client: client,
-        verb: verb,
-        path_and_query: path_and_query,
-        path: path,
+        client,
+        verb,
+        path_and_query,
+        path,
         _query: query,
         status: 0,
     };
@@ -97,12 +90,12 @@ fn handle_request(client: &mut ClientConnection) -> std::io::Result<()> {
     Ok(())
 }
 
-fn handle_static_file(mut http_context: &mut HttpContext) -> std::io::Result<()> {
+fn handle_static_file(http_context: &mut HttpContext) -> std::io::Result<()> {
     let mut path_buf = PathBuf::from(WWWROOT);
     path_buf.push(http_context.path.trim_start_matches('/'));
     match File::open(path_buf) {
-        Ok(file) => { send_file(&mut http_context, file)?; },
-        Err(_) => { send_404(&mut http_context)?; }
+        Ok(file) => { send_file(http_context, file)?; },
+        Err(_) => { send_404(http_context)?; }
     };
     Ok(())
 }
@@ -113,10 +106,10 @@ fn send_file(http_context: &mut HttpContext, mut file: File) -> std::io::Result<
         http_context.status = 200;
         let mut filebuf: Vec<u8> = Vec::new();
         file.read_to_end(&mut filebuf)?;
-        stream.write(b"HTTP/1.1 200 OK\r\n")?;
-        stream.write(format!("Content-Type: {}\r\n", content_type).as_bytes())?;
-        stream.write(format!("Content-Length: {}\r\n", filebuf.len()).as_bytes())?;
-        stream.write(b"\r\n")?;
+        stream.write_all(b"HTTP/1.1 200 OK\r\n")?;
+        stream.write_all(format!("Content-Type: {}\r\n", content_type).as_bytes())?;
+        stream.write_all(format!("Content-Length: {}\r\n", filebuf.len()).as_bytes())?;
+        stream.write_all(b"\r\n")?;
         stream.write_all(&filebuf)?;
         Ok(())
     } else {
@@ -140,9 +133,9 @@ fn send_405(http_context: &mut HttpContext) -> std::io::Result<()> {
 }
 
 fn send_message(mut stream: &TcpStream, status_code: u16, status_msg: &str, msg: &str) -> std::io::Result<()> {
-    stream.write(format!("HTTP/1.1 {} {}\r\n", status_code, status_msg).as_bytes())?;
-    stream.write(format!("Content-Type: text/html\r\n").as_bytes())?;
-    stream.write(format!("Content-Length: {}\r\n\r\n{}", msg.len(), msg).as_bytes())?;
+    stream.write_all(format!("HTTP/1.1 {} {}\r\n", status_code, status_msg).as_bytes())?;
+    stream.write_all(b"Content-Type: text/html\r\n")?;
+    stream.write_all(format!("Content-Length: {}\r\n\r\n{}", msg.len(), msg).as_bytes())?;
     Ok(())
 }
 
@@ -150,25 +143,25 @@ fn _echo_client(mut stream: TcpStream ) -> std::io::Result<()> {
     let mut buf: [u8; 4096] = [0; 4096];
     let bytes_read = stream.read(&mut buf)?;
     println!("{}", bytes_read);
-    stream.write(b"HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n")?;
-    stream.write(format!("Content-Length: {}\r\n\r\n", bytes_read + 11).as_bytes())?;
-    stream.write(b"<pre>")?;
-    stream.write(&buf[..bytes_read])?;
-    stream.write(b"</pre>")?;
+    stream.write_all(b"HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n")?;
+    stream.write_all(format!("Content-Length: {}\r\n\r\n", bytes_read + 11).as_bytes())?;
+    stream.write_all(b"<pre>")?;
+    stream.write_all(&buf[..bytes_read])?;
+    stream.write_all(b"</pre>")?;
     Ok(())
 }
 
 fn content_type_from_path_extension(path: &str) -> Option<&str> {
     if let Some(ext_index) = path.rfind('.') {
         let (_, ext) = path.split_at(ext_index);
-        return match ext {
+        match ext {
             ".js" => Some("text/javascript"),
             ".css" => Some("text/css"),
             ".html" => Some("text/html"),
             _ => None,
         }
     } else {
-        return None;
+        None
     }
 }
 
