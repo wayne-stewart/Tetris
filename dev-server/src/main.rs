f8ad04fc0c9b0958aa7607e96983eb33070c1981
Wayne Stewart <waynegaidin@hotmail.com>
2025-11-29 21:43:03 -0600

    factored out getting content-type from file extension

--- dev-server/src/main.rs
+++ dev-server/src/main.rs
@@ -46,7 +46,6 @@ fn main() -> std::io::Result<()> {
 
 fn handle_client(mut client: ClientConnection) {
     client.stream.set_read_timeout(Some(Duration::from_secs(5))).unwrap();
-    //client.stream.set_nonblocking(true);
     while client.keep_alive {
         client.request_count += 1;
         match handle_request(&mut client) {
@@ -80,7 +79,6 @@ fn handle_request(client: &mut ClientConnection) -> std::io::Result<()> {
     if let Some(query_index) = path_and_query.find('?') {
         (path, query) = path_and_query.split_at(query_index);
     }
-    //println!("verb: {}, path: {}, http: {}", verb, path, http_version);
     let mut http_context = HttpContext {
         client: client,
         verb: verb,
@@ -111,22 +109,19 @@ fn handle_static_file(mut http_context: &mut HttpContext) -> std::io::Result<()>
 
 fn send_file(http_context: &mut HttpContext, mut file: File) -> std::io::Result<()> {
     let mut stream = &http_context.client.stream;
-    let content_type = match http_context.path.split(".").last().unwrap() {
-        "js" => "text/javascript",
-        "css" => "text/css",
-        "html" => "text/html",
-        _ => "",
-    };
-    if content_type.len() == 0 { send_400(http_context, "file extension not supported!")?; return Ok(()); }
-    http_context.status = 200;
-    let mut filebuf: Vec<u8> = Vec::new();
-    file.read_to_end(&mut filebuf)?;
-    stream.write(b"HTTP/1.1 200 OK\r\n")?;
-    stream.write(format!("Content-Type: {}\r\n", content_type).as_bytes())?;
-    stream.write(format!("Content-Length: {}\r\n", filebuf.len()).as_bytes())?;
-    stream.write(b"\r\n")?;
-    stream.write_all(&filebuf)?;
-    Ok(())
+    if let Some(content_type) = content_type_from_path_extension(http_context.path) {
+        http_context.status = 200;
+        let mut filebuf: Vec<u8> = Vec::new();
+        file.read_to_end(&mut filebuf)?;
+        stream.write(b"HTTP/1.1 200 OK\r\n")?;
+        stream.write(format!("Content-Type: {}\r\n", content_type).as_bytes())?;
+        stream.write(format!("Content-Length: {}\r\n", filebuf.len()).as_bytes())?;
+        stream.write(b"\r\n")?;
+        stream.write_all(&filebuf)?;
+        Ok(())
+    } else {
+        send_400(http_context, "file extension not supported!")
+    }
 }
 
 fn send_400(http_context: &mut HttpContext, msg: &str) -> std::io::Result<()> {
@@ -146,7 +141,6 @@ fn send_405(http_context: &mut HttpContext) -> std::io::Result<()> {
 
 fn send_message(mut stream: &TcpStream, status_code: u16, status_msg: &str, msg: &str) -> std::io::Result<()> {
     stream.write(format!("HTTP/1.1 {} {}\r\n", status_code, status_msg).as_bytes())?;
-    stream.write(b"Connection: keep-alive\r\n")?;
     stream.write(format!("Content-Type: text/html\r\n").as_bytes())?;
     stream.write(format!("Content-Length: {}\r\n\r\n{}", msg.len(), msg).as_bytes())?;
     Ok(())
@@ -164,8 +158,25 @@ fn _echo_client(mut stream: TcpStream ) -> std::io::Result<()> {
     Ok(())
 }
 
+fn content_type_from_path_extension(path: &str) -> Option<&str> {
+    if let Some(ext_index) = path.rfind('.') {
+        let (_, ext) = path.split_at(ext_index);
+        return match ext {
+            ".js" => Some("text/javascript"),
+            ".css" => Some("text/css"),
+            ".html" => Some("text/html"),
+            _ => None,
+        }
+    } else {
+        return None;
+    }
+}
+
 fn log_error(msg: &str) {
     println!("ERROR: {}", msg);
+}
+fn _log_debug(msg: &str) {
+    println!("DEBUG: {}", msg);
 }
 fn log_request(ctx: &HttpContext) {
     println!("{} {} {} {}", 
